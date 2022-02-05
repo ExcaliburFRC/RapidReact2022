43e7f299678bba632f2dfc3c7f30d35e5a7e9d4e
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2022-02-05 22:51:22 +0200

    Add RepeatingCommand (#43)

--- src/main/java/io/excaliburfrc/robot/commands/ShootBallsCommand.java
+++ src/main/java/io/excaliburfrc/robot/commands/ShootBallsCommand.java
@@ -1,22 +1,28 @@
 package io.excaliburfrc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import io.excaliburfrc.lib.RepeatingCommand;
 import io.excaliburfrc.robot.subsystems.Intake;
 import io.excaliburfrc.robot.subsystems.Shooter;
 
 public class ShootBallsCommand extends ParallelDeadlineGroup {
-  public ShootBallsCommand(Intake intake, Shooter shooter) {
+  /**
+   * @param trigger an additional trigger, will release a ball only when this (and the velocity
+   *     check) are reached.
+   */
+  public ShootBallsCommand(Intake intake, Shooter shooter, Trigger trigger) {
     super(
         // FIXME: handle mechanism failures? (ie stuck balls, etc)
         new WaitUntilCommand(intake::isEmpty),
         shooter.accelerateFenderCommand(),
-        // motor
+        new RepeatingCommand(
+            new SequentialCommandGroup(
+                new WaitUntilCommand(new Trigger(shooter::isAtTargetVelocity).and(trigger)),
+                intake.shootBallCommand())));
+  }
 
-        // TODO: check perpetuating. for now, duplicating is fine
-        new SequentialCommandGroup(
-            new WaitUntilCommand(shooter::isAtTargetVelocity),
-            intake.shootBallCommand(),
-            new WaitUntilCommand(shooter::isAtTargetVelocity),
-            intake.shootBallCommand()));
+  public ShootBallsCommand(Intake intake, Shooter shooter) {
+    this(intake, shooter, new Trigger(() -> true));
   }
 }
