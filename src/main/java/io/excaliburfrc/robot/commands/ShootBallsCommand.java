809574f103577b56cd349ab1426b2be4be37c4e7
ExcaliburFRC <excaliburfrc@gmail.com>
2022-02-06 21:45:06 +0200

    adding leds to the ShootBallsCommand

--- src/main/java/io/excaliburfrc/robot/commands/ShootBallsCommand.java
+++ src/main/java/io/excaliburfrc/robot/commands/ShootBallsCommand.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import io.excaliburfrc.lib.RepeatingCommand;
 import io.excaliburfrc.robot.subsystems.Intake;
+import io.excaliburfrc.robot.subsystems.LEDs;
 import io.excaliburfrc.robot.subsystems.Shooter;
 
 public class ShootBallsCommand extends ParallelDeadlineGroup {
@@ -11,7 +12,7 @@ public class ShootBallsCommand extends ParallelDeadlineGroup {
    * @param trigger an additional trigger, will release a ball only when this (and the velocity
    *     check) are reached.
    */
-  public ShootBallsCommand(Intake intake, Shooter shooter, Trigger trigger) {
+  public ShootBallsCommand(Intake intake, Shooter shooter, LEDs leds, Trigger trigger) {
     super(
         // FIXME: handle mechanism failures? (ie stuck balls, etc)
         new WaitUntilCommand(intake::isEmpty),
@@ -19,10 +20,11 @@ public class ShootBallsCommand extends ParallelDeadlineGroup {
         new RepeatingCommand(
             new SequentialCommandGroup(
                 new WaitUntilCommand(new Trigger(shooter::isAtTargetVelocity).and(trigger)),
-                intake.shootBallCommand())));
+                intake.shootBallCommand())),
+        LEDs.getInstance().setColorCommand(LEDs.LedMode.BLUE));
   }
 
-  public ShootBallsCommand(Intake intake, Shooter shooter) {
-    this(intake, shooter, new Trigger(() -> true));
+  public ShootBallsCommand(Intake intake, Shooter shooter, LEDs leds) {
+    this(intake, shooter, leds, new Trigger(() -> true));
   }
 }
