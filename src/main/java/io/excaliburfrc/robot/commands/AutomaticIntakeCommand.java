98538f438b1c5ce7f9dacf908adf27f079434ad5
Ariel Cohen <74930666+CohenAriel@users.noreply.github.com>
2022-02-20 21:09:06 +0200

    spotless

--- src/main/java/io/excaliburfrc/robot/commands/AutomaticIntakeCommand.java
+++ src/main/java/io/excaliburfrc/robot/commands/AutomaticIntakeCommand.java
@@ -2,7 +2,6 @@ package io.excaliburfrc.robot.commands;
 
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import io.excaliburfrc.robot.subsystems.Drive;
 import io.excaliburfrc.robot.subsystems.Intake;
