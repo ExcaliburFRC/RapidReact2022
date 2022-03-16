d96fc68341a04deedc7f996457ca65dbbdbc31e6
Shai Grossman <shaigimel@gmail.com>
2022-03-16 23:32:49 +0200

    tune climb soft limits and inversions

--- src/main/java/io/excaliburfrc/robot/subsystems/Climber.java
+++ src/main/java/io/excaliburfrc/robot/subsystems/Climber.java
@@ -17,22 +17,26 @@ import edu.wpi.first.wpilibj2.command.*;
 import io.excaliburfrc.robot.Constants.ClimberConstants;
 import java.util.function.BooleanSupplier;
 
-public class Climber extends SubsystemBase implements AutoCloseable {
+public class Climber extends SubsystemBase {
   private final DoubleSolenoid anglerPiston =
       new DoubleSolenoid(
           PneumaticsModuleType.CTREPCM,
           ClimberConstants.FORWARD_CHANNEL,
           ClimberConstants.REVERSE_CHANNEL);
-  private final ClimberSide left = new ClimberSide(LEFT_MOTOR_ID, false);
-  private final ClimberSide right = new ClimberSide(RIGHT_MOTOR_ID, true);
 
-  private static class ClimberSide implements AutoCloseable {
+  private final ClimberSide left = new ClimberSide(LEFT_MOTOR_ID, true, 0.95);
+  private final ClimberSide right = new ClimberSide(RIGHT_MOTOR_ID, false, 0.85);
+
+  private static class ClimberSide {
     private final CANSparkMax motor;
     private final RelativeEncoder encoder;
 
-    public ClimberSide(int motorId, boolean isMotorReversed) {
+    private final double Mspeed;
+
+    public ClimberSide(int motorId, boolean isMotorReversed, double Mspeed) {
       motor = new CANSparkMax(motorId, MotorType.kBrushless);
       encoder = motor.getEncoder();
+      this.Mspeed = Mspeed;
 
       ValidateREVCAN(
           // reset factory settings
@@ -42,42 +46,13 @@ public class Climber extends SubsystemBase implements AutoCloseable {
           motor.setPeriodicFramePeriod(PeriodicFrame.kStatus0, StatusFramePeriods.DEFAULT),
           motor.setPeriodicFramePeriod(PeriodicFrame.kStatus1, StatusFramePeriods.DO_NOT_SEND),
           motor.setPeriodicFramePeriod(PeriodicFrame.kStatus2, StatusFramePeriods.DEFAULT),
-          motor.setSoftLimit(SoftLimitDirection.kReverse, 0),
+          motor.setSoftLimit(SoftLimitDirection.kReverse, REVERSE_SOFT_LIMIT),
           motor.enableSoftLimit(SoftLimitDirection.kReverse, true),
           motor.setSoftLimit(SoftLimitDirection.kForward, ClimberConstants.FORWARD_SOFT_LIMIT),
-          motor.enableSoftLimit(SoftLimitDirection.kForward, false));
+          motor.enableSoftLimit(SoftLimitDirection.kForward, true));
       motor.setInverted(isMotorReversed);
 
       encoder.setPosition(0);
-    }
-
-    public Command pullUpCommand() {
-      return new FunctionalCommand(
-          () -> {},
-          () -> motor.set(-OPEN_LOOP_CLIMB_DUTY_CYCLE),
-          __ -> motor.set(0),
-          () -> encoder.getPosition() <= CLOSED_HEIGHT);
-    }
-
-    public Command pullUpHalfCommand() {
-      return new FunctionalCommand(
-          () -> {},
-          () -> motor.set(-OPEN_LOOP_CLIMB_DUTY_CYCLE),
-          __ -> motor.set(0),
-          () -> encoder.getPosition() <= HALF_HEIGHT);
-    }
-
-    public Command openFullyCommand() {
-      return new FunctionalCommand(
-          () -> {},
-          () -> motor.set(OPEN_LOOP_CLIMB_DUTY_CYCLE),
-          __ -> motor.set(0),
-          () -> encoder.getPosition() >= OPEN_HEIGHT);
-    }
-
-    /** AutoCloseable */
-    public void close() {
-      motor.close();
     }
 
     public Command manualCommand(BooleanSupplier up, BooleanSupplier down) {
@@ -85,13 +60,10 @@ public class Climber extends SubsystemBase implements AutoCloseable {
           () -> {},
           () -> {
             if (up.getAsBoolean()) {
-              motor.set(0.9);
-              System.out.println("up");
+              motor.set(Mspeed / 4);
             } else if (down.getAsBoolean()) {
-              System.out.println("down");
-              motor.set(-0.9);
+              motor.set(-Mspeed);
             } else {
-              System.out.println("nil");
               motor.set(0);
             }
           },
@@ -127,70 +99,11 @@ public class Climber extends SubsystemBase implements AutoCloseable {
             motor.enableSoftLimit(SoftLimitDirection.kReverse, false);
           },
           () -> {
-            //            encoder.setPosition(0);
+            encoder.setPosition(0);
             motor.enableSoftLimit(SoftLimitDirection.kForward, true);
             motor.enableSoftLimit(SoftLimitDirection.kReverse, true);
           });
     }
-  }
-
-  /** AutoCloseable */
-  @Override
-  public void close() {
-    left.close();
-    right.close();
-    anglerPiston.close();
-  }
-
-  public Command openFullyCommand() {
-    return new ParallelCommandGroup(left.openFullyCommand(), right.openFullyCommand());
-  }
-
-  public Command pullUpRobotCommand() {
-    return left.pullUpCommand().alongWith(right.pullUpCommand());
-  }
-
-  public Command pullUpHalfRobotCommand() {
-    return left.pullUpHalfCommand().alongWith(right.pullUpHalfCommand());
-  }
-
-  public Command openAnglerCommand() {
-    return new InstantCommand(() -> anglerPiston.set(ANGLED), this);
-  }
-
-  public Command straightenAnglerCommand() {
-    return new InstantCommand(() -> anglerPiston.set(STRAIGHT), this);
-  }
-
-  public Command climbSeries(
-      BooleanSupplier openFirst,
-      BooleanSupplier closeFirst,
-      BooleanSupplier openSecond,
-      BooleanSupplier angleSecond,
-      BooleanSupplier closeSecond,
-      BooleanSupplier closeAngle) {
-    return new SequentialCommandGroup(
-            new PrintCommand("Started Climb Series"),
-            new WaitUntilCommand(openFirst),
-            new PrintCommand("Started Climb Series 2"),
-            openFullyCommand(),
-            new WaitUntilCommand(closeFirst),
-            new PrintCommand("Started Climb Series 3"),
-            pullUpRobotCommand(),
-            new WaitUntilCommand(openSecond),
-            new PrintCommand("Started Climb Series 4"),
-            openFullyCommand(),
-            new WaitUntilCommand(angleSecond),
-            new PrintCommand("Started Climb Series 5"),
-            openAnglerCommand(),
-            new WaitUntilCommand(() -> !angleSecond.getAsBoolean()),
-            new WaitUntilCommand(closeSecond),
-            new PrintCommand("Started Climb Series 6"),
-            pullUpHalfRobotCommand(),
-            new WaitUntilCommand(closeAngle),
-            new PrintCommand("Started Climb Series 7"),
-            straightenAnglerCommand())
-        .withName("climb series");
   }
 
   public Command climberManualCommand(
@@ -201,20 +114,9 @@ public class Climber extends SubsystemBase implements AutoCloseable {
       BooleanSupplier pistonAngled,
       BooleanSupplier pistonStraight) {
     return new ParallelCommandGroup(
-        left.manualCommand(leftUp, leftDown),
-        right.manualCommand(rightUp, rightDown),
-        new FunctionalCommand(
-            () -> {},
-            () -> {
-              if (pistonAngled.getAsBoolean()) {
-                anglerPiston.set(ANGLED);
-              }
-              if (pistonStraight.getAsBoolean()) {
-                anglerPiston.set(STRAIGHT);
-              }
-            },
-            __ -> {},
-            () -> false));
+         left.manualCommand(leftUp, leftDown),
+                   right.manualCommand(rightUp, rightDown),
+            pistonCommand(pistonAngled, pistonStraight));
   }
 
   public Command climberTuneCommand(
@@ -227,7 +129,11 @@ public class Climber extends SubsystemBase implements AutoCloseable {
     return new ParallelCommandGroup(
         left.tuneCommand(leftUp, leftDown),
         right.tuneCommand(rightUp, rightDown),
-        new FunctionalCommand(
+pistonCommand(pistonAngled, pistonStraight));
+  }
+
+  private FunctionalCommand pistonCommand(BooleanSupplier pistonAngled, BooleanSupplier pistonStraight) {
+    return new FunctionalCommand(
             () -> {},
             () -> {
               if (pistonAngled.getAsBoolean()) {
@@ -238,7 +144,7 @@ public class Climber extends SubsystemBase implements AutoCloseable {
               }
             },
             __ -> {},
-            () -> false));
+            () -> false);
   }
 
   public Command disableSoftLimits() {
