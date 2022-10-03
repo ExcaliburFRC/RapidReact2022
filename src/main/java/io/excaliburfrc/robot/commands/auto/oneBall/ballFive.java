dc4fdc176aac887ba1966422baa84192861c4f7d
Excalibur FRC | 6738 <excaliburfrc@gmail.com>
2022-10-03 20:31:15 +0300

    merge

--- src/main/java/io/excaliburfrc/robot/commands/auto/oneBall/ballFive.java
+++ src/main/java/io/excaliburfrc/robot/commands/auto/oneBall/ballFive.java
@@ -3,6 +3,7 @@ package io.excaliburfrc.robot.commands.auto.oneBall;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import io.excaliburfrc.robot.commands.auto.Trajectories;
@@ -13,17 +14,19 @@ import io.excaliburfrc.robot.subsystems.Superstructure;
 import static io.excaliburfrc.robot.commands.auto.Trajectories.*;
 
 public class ballFive extends SequentialCommandGroup {
-  static final Pose2d start = new Pose2d(6.952, 2.509, Rotation2d.fromDegrees(200));
-  static final Pose2d end = new Pose2d(7.9, 2.6, Rotation2d.fromDegrees(55));
+  static final Pose2d start = new Pose2d(6.952, 2.509, Rotation2d.fromDegrees(60));
   static final Translation2d ball1 = Trajectories.OUR_CARGO_5;
 
   public ballFive(Drive drive, LEDs leds, Superstructure superstructure) {
     super(
           drive.resetOdometryCommand(start),
-          drive.followTrajectoryCommand(TrajectoryGenerator.generateTrajectory(
-                start, innerWaypoints(ball1), end, FORWARD)
-          ).alongWith(superstructure.intakeBallCommand())
-                .andThen(superstructure.shootBallsCommand(leds))
-    );
+          superstructure.shootBallsCommand(leds),
+          drive.arcadeDriveCommand(() -> 0, () -> -0.3)
+                .until(()-> drive.getDegrees() > -140 && drive.getDegrees() < -130),
+          drive.followTrajectoryCommand(
+                drive.getOdometryPose(), innerWaypoints(ball1), start, FORWARD)
+                .alongWith(superstructure.intakeBallCommand()),
+          superstructure.shootBallsCommand(leds));
+
   }
 }
--- src/main/java/io/excaliburfrc/robot/commands/auto/twoBalls/fiveFour.java
+++ src/main/java/io/excaliburfrc/robot/commands/auto/twoBalls/fiveFour.java
@@ -1,13 +1,13 @@
 package io.excaliburfrc.robot.commands.auto.twoBalls;
 
-import static io.excaliburfrc.robot.commands.auto.Trajectories.OUR_CARGO_4;
-import static io.excaliburfrc.robot.commands.auto.Trajectories.OUR_CARGO_5;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 import static edu.wpi.first.math.geometry.Rotation2d.fromDegrees;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import io.excaliburfrc.robot.commands.auto.Trajectories;
 import io.excaliburfrc.robot.subsystems.Drive;
 import io.excaliburfrc.robot.subsystems.LEDs;
 import io.excaliburfrc.robot.subsystems.Superstructure;
@@ -15,33 +15,21 @@ import io.excaliburfrc.robot.subsystems.Superstructure;
 import static io.excaliburfrc.robot.commands.auto.Trajectories.*;
 
 public class fiveFour extends SequentialCommandGroup {
-	static final Pose2d START = new Pose2d(8.2, 2.40, fromDegrees(90));
-	static final Pose2d BALL_1 = new Pose2d(OUR_CARGO_5.getX(), OUR_CARGO_5.getY(), fromDegrees(200));
-	static final Pose2d BALL_2 = new Pose2d(OUR_CARGO_4.getX(), OUR_CARGO_5.getY(), fromDegrees(0));
-	static final Pose2d STOP_1 = new Pose2d(8.168, 1.676, START.getRotation());
+static final Pose2d START = new Pose2d(8.2, 2.40, Rotation2d.fromDegrees(90));
+  static final Pose2d ball1 = new Pose2d(Trajectories.OUR_CARGO_5.getX(), Trajectories.OUR_CARGO_5.getY(), fromDegrees(200));
+  static final Pose2d ball2 = new Pose2d(Trajectories.OUR_CARGO_4.getX(), Trajectories.OUR_CARGO_4.getY(), fromDegrees(0));
+  static final Pose2d STOP1 = new Pose2d(8.168, 1.676,START.getRotation());
 
-	static final Trajectory firstTraj =
-			TrajectoryGenerator.generateTrajectory(START, innerWaypoints(), STOP_1, REVERSE);
-
-	static final Trajectory secondTraj =
-			TrajectoryGenerator.generateTrajectory(STOP_1, innerWaypoints(), BALL_1, FORWARD);
-
-	static final Trajectory thirdTraj =
-			TrajectoryGenerator.generateTrajectory(BALL_1, innerWaypoints(), BALL_2, FORWARD);
-
-	static final Trajectory fourthTraj =
-			TrajectoryGenerator.generateTrajectory(BALL_2, innerWaypoints(), START, FORWARD);
-
-	public fiveFour(Drive drive, LEDs leds, Superstructure superstructure) {
-		super(
-				drive.resetOdometryCommand(START),
-				superstructure.shootBallsCommand(leds),
-				drive.followTrajectoryCommand(firstTraj),
-				new SequentialCommandGroup(
-                        drive.followTrajectoryCommand(secondTraj),
-                        drive.followTrajectoryCommand(thirdTraj),
-						drive.followTrajectoryCommand(fourthTraj)
-						).alongWith(superstructure.intakeBallCommand(), superstructure.intakeBallCommand()),
-				superstructure.shootBallsCommand(leds));
-	}
+  public fiveFour(Drive drive, LEDs leds, Superstructure superstructure) {
+      super(
+            drive.resetOdometryCommand(START),
+            superstructure.shootBallsCommand(leds),
+            drive.followTrajectoryCommand(START, innerWaypoints(), STOP1, REVERSE),
+            new SequentialCommandGroup(
+                  drive.followTrajectoryCommand(STOP1, innerWaypoints(), ball1, FORWARD),
+                  drive.followTrajectoryCommand(ball1, innerWaypoints(), ball2, FORWARD),
+                  drive.followTrajectoryCommand(ball2, innerWaypoints(), START, FORWARD)
+                  ).alongWith(superstructure.intakeBallCommand().andThen(superstructure.intakeBallCommand())),
+            superstructure.shootBallsCommand(leds));
+    }
 }
