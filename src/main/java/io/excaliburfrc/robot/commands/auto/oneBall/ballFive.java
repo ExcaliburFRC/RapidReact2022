41157ea6815e1fdc38c381a60a90c46de26ccf61
Excalibur FRC | 6738 <excaliburfrc@gmail.com>
2022-10-03 20:25:41 +0300

    coded ballFive auto

--- src/main/java/io/excaliburfrc/robot/commands/auto/oneBall/ballFive.java
+++ src/main/java/io/excaliburfrc/robot/commands/auto/oneBall/ballFive.java
@@ -14,17 +14,18 @@ import io.excaliburfrc.robot.subsystems.Superstructure;
 import static io.excaliburfrc.robot.commands.auto.Trajectories.*;
 
 public class ballFive extends SequentialCommandGroup {
-  static final Pose2d start = new Pose2d(6.952, 2.509, Rotation2d.fromDegrees(200));
-  static final Pose2d end = new Pose2d(7.9, 2.6, Rotation2d.fromDegrees(55));
+  static final Pose2d start = new Pose2d(6.952, 2.509, Rotation2d.fromDegrees(60));
   static final Translation2d ball1 = Trajectories.OUR_CARGO_5;
 
   public ballFive(Drive drive, LEDs leds, Superstructure superstructure) {
     super(
           drive.resetOdometryCommand(start),
-          drive.followTrajectoryCommand(TrajectoryGenerator.generateTrajectory(
-                start, innerWaypoints(ball1), end, FORWARD())
-          ).alongWith(superstructure.intakeBallCommand())
-                .andThen(superstructure.shootBallsCommand(leds))
-    );
+          superstructure.shootBallsCommand(leds),
+          drive.arcadeDriveCommand(() -> 0, () -> -0.3)
+                .until(()-> drive.getDegrees() > -140 && drive.getDegrees() < -130),
+          drive.followTrajectoryCommand(drive.getOdometryPose(), innerWaypoints(ball1), start, FORWARD())
+                .alongWith(superstructure.intakeBallCommand()),
+          superstructure.shootBallsCommand(leds));
+
   }
 }
