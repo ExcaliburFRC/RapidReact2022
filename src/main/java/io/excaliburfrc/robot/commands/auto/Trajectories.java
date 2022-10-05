002deb9ae787b63ae3998e235e20a444d3d9f8ac
Korozya-bit <shaigimel@gmail.com>
2022-10-05 22:55:58 +0300

    Revert "fixed OUR_BALL_6 location"
    This reverts commit 70df2e6b2a6ef89f165653b6773e7e0cc6a62431.

--- src/main/java/io/excaliburfrc/robot/commands/auto/Trajectories.java
+++ src/main/java/io/excaliburfrc/robot/commands/auto/Trajectories.java
@@ -23,11 +23,11 @@ public enum Trajectories {
   public static final Translation2d OUR_CARGO_4 = new Translation2d(7.7, 0.3);
   public static final Translation2d OUR_CARGO_5 = new Translation2d(5.18, 1.9);
   public static final Translation2d THEIR_CARGO_5 = new Translation2d(4.55, 3.25);
-  public static final Translation2d OUR_CARGO_6 = new Translation2d(5.01, 6.11);
+  public static final Translation2d OUR_CARGO_6 = new Translation2d(15.55, 7.12);
   public static final Translation2d THEIR_CARGO_6 = new Translation2d(6.1, 7.3);
 
-  public static final Translation2d OUR_TERMINAL_CARGO = new Translation2d(1.15, 1.11);
-  public static final Translation2d THEIR_TERMINAL_CARGO = new Translation2d(15.519, 7.063);
+  public static final Translation2d OUR_TERMINAL_CARGO = new Translation2d(1.15, 1.115);
+  public static final Translation2d THEIR_TERMINAL_CARGO = new Translation2d(1.15, 1.115);
 
   public static Trajectory generateTrajectory(
       Pose2d start, List<Translation2d> innerWaypoints, Pose2d end, TrajectoryConfig config) {
